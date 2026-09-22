1a0afe6340adc1c0b509b3bc69ab4a034af29d20
cyang3333333333 <connoryyang@gmail.com>
2026-09-22 09:06:55 -0700

    u told me to commit

--- Git.java
+++ Git.java
@@ -3,32 +3,33 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.security.MessageDigest;
 
 
 public class Git {
     public static void main(String[] args) throws IOException {
         init();
-        System.out.println(hashFile("README.md"));
+        blob("README.md");
         
     }
 
     public static void init() {
         int exist = 0;
-        File git1 = new File("git");
+        File git1 = new File(".git");
         if (git1.exists() == false) {
             git1.mkdir();
 
         } else {
             exist+=1;
         }
-        File git2 = new File("git/objects");
+        File git2 = new File(".git/objects");
         if (git2.exists() == false) {
             git2.mkdir();
         } else {
             exist+=1;
         }
-        File git3 = new File("git/index");
+        File git3 = new File(".git/index");
         if (git3.exists() == false) {
             try {
                 git3.createNewFile();   
@@ -38,7 +39,7 @@ public class Git {
         } else {
             exist+=1;
         }
-        File git4 = new File("git/HEAD");
+        File git4 = new File(".git/HEAD");
         if (git4.exists() == false) {
             try {
                 git4.createNewFile();   
@@ -63,12 +64,11 @@ public class Git {
         } catch (IOException e) {
             System.out.println("cant");
         }
-        MessageDigest digest = null;
         try {
-            digest = MessageDigest.getInstance("SHA-1");
-            byte[] encodedHash = digest.digest(str.getBytes());
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hash = digest.digest(str.getBytes());
             StringBuilder hexString = new StringBuilder();
-                for (byte b : encodedHash) {
+                for (byte b : hash) {
                     String hex = String.format("%02x", b);
                     if (hex.length() == 1) {
                         hexString.append('0');
@@ -80,5 +80,26 @@ public class Git {
             System.out.println("cant missing file");
             return null;
         }
+    }
+
+    public static void blob(String filePath) throws IOException {
+        String fileName = hashFile(filePath);
+        String newpath = ".git/objects/" + fileName;
+        File hashed = new File(newpath);
+        hashed.createNewFile();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            try (FileWriter writer = new FileWriter(newpath)) {
+                String result = reader.readAllAsString();
+                writer.write(result);
+                reader.close();
+                writer.close();
+            } catch (Exception e) {
+                System.out.println("cant");
+            }
+        } catch (Exception e) {
+                System.out.println("cant");
+        }
+
+
     }
 }
